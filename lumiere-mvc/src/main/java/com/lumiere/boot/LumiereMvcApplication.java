d8cb852efb40ec0fcb68a0a4707bafb60dabf8e7
diogo262 <thenolance10@gmail.com>
2023-10-17 20:55:28 -0300

    SpringBoot MVC

---
+++ lumiere-mvc/src/main/java/com/lumiere/boot/LumiereMvcApplication.java
@@ -0,0 +1,13 @@
+package com.lumiere.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiereMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumiereMvcApplication.class, args);
+	}
+
+}
---
+++ lumiere-mvc/src/test/java/com/lumiere/boot/LumiereMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumiere.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumiereMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
