1ea5a80beef4ef80d9864c28eef02613d4848159
diogo262 <thenolance10@gmail.com>
2023-10-24 21:56:05 -0300

    Link para o banco de dados

--- lumiere-mvc/src/main/java/com/lumiere/boot/web/controller/IndexController.java
+++ lumiere-mvc/src/main/java/com/lumiere/boot/web/controller/IndexController.java
@@ -2,10 +2,12 @@ package com.lumiere.boot.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 // Controller para a pagina index
 
 @Controller
+@RequestMapping("/index")
 public class IndexController {
 	
 	@GetMapping("/")
