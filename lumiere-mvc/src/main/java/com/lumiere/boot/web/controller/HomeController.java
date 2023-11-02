9ba8a073a8de64fbfecf42c433728ac8188dae8a
Eric Carvalho <ericcarvalho935@gmail.com>
2023-11-01 23:50:45 -0300

    Implementação parcial da home
    Implementação parcial da estilização da home

--- lumiere-mvc/src/main/java/com/lumiere/boot/web/controller/HomeController.java
+++ lumiere-mvc/src/main/java/com/lumiere/boot/web/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.lumiere.boot.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 
 @Controller
@@ -11,4 +12,10 @@ public class HomeController {
 	public String home() {
 		return "/home";
 	}
+	
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/resources/**")
+                    .addResourceLocations("/public", "classpath:/static/")
+                    .setCachePeriod(31556926);
+    }
 }
