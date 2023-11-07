56f67c747050cb831bc2486b07ae29bfcbe9217b
diogo262 <thenolance10@gmail.com>
2023-11-07 20:15:58 -0300

    Tentando listar residencias

--- lumiere-mvc/src/main/java/com/lumiere/boot/service/ResidenciaServiceImpl.java
+++ lumiere-mvc/src/main/java/com/lumiere/boot/service/ResidenciaServiceImpl.java
@@ -21,10 +21,10 @@ public class ResidenciaServiceImpl implements ResidenciaService {
 		residenciaDao.save(residencia);
 	}
 
-	@Override
-	public List<Residencia> buscarResidenciaPorUsuario(int cdUsuario) {
-		return residenciaDao.buscarResidenciaPorUsuario(cdUsuario);
-	}
+	//@Override
+	//public List<Residencia> buscarResidenciaPorUsuario(int cdUsuario) {
+		//return residenciaDao.buscarResidenciaPorUsuario(cdUsuario);
+	//}
 	
 	@Override
 	public List<Residencia> buscarTodos() {
