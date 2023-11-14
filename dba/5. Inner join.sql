29dfa1b9e0df41e00cb9ad427c280bbe59f3aa15
Eric Carvalho <ericcarvalho935@gmail.com>
2023-11-14 03:39:56 -0300

    Adição de scripts SQL
    Adição dos futuros scripts que serão utilizados nos relatórios

--- dba/5. Inner join.sql
+++ dba/5. Inner join.sql
@@ -1,7 +1,33 @@
 use lumieredb;
 
+-- --------------------------- --
+--        Inner joins   	   --
+-- --------------------------- --
+
+/* Select para consultar o consumo total por mes em um periodo X */
+
+SELECT 
+	YEAR(c.data_consumo) as ano,
+	MONTH(c.data_consumo) as mes,
+	SUM(c.preco_consumo) as consumoTotal
+FROM 
+	Consumo as c
+INNER JOIN 
+	Dispositivo as d
+ON 
+	c.fk_Dispositivo_cd_dispositivo = d.cd_dispositivo
+WHERE 
+	d.fk_Residencia_cd_residencia = 1
+GROUP BY 
+	YEAR(c.data_consumo),
+	MONTH(c.data_consumo)
+ORDER BY 
+	YEAR(c.data_consumo) DESC
+
+/* Select para consultar o consumo total em um ano X */
+
 SELECT
-	sum(preco_consumo)
+	SUM(preco_consumo) as consumoTotal
 FROM 
 	Consumo as c
 INNER JOIN 
@@ -10,4 +36,20 @@ ON
 	c.fk_Dispositivo_cd_dispositivo = d.cd_dispositivo
 WHERE 
 	d.fk_Residencia_cd_residencia = 1 and
-	year(c.data_consumo) = 2023
+	YEAR(c.data_consumo) = 2023
+
+/* Select para consultar o consumo total em um periodo de ateh 7 dias */
+
+SELECT 
+	c.data_consumo,
+	SUM(preco_consumo) as consumoTotal
+FROM 
+	Consumo as c
+INNER JOIN 
+	Dispositivo as d
+ON 
+	c.fk_Dispositivo_cd_dispositivo = d.cd_dispositivo
+WHERE 
+	c.data_consumo >= DATEADD(day,-7, GETDATE())
+GROUP BY
+	c.data_consumo
