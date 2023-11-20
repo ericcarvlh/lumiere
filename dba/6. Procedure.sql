9bb4471d196c5f5b76a65b7662c1eea029fcd86e
diogo262 <thenolance10@gmail.com>
2023-11-19 23:40:48 -0300

    Comeco da procedure para relatorio semanal

--- dba/6. Procedure.sql
+++ dba/6. Procedure.sql
@@ -85,3 +85,57 @@ WHERE
 	c.data_consumo >= DATEADD(day, -60, GETDATE())
 
 EXEC sp_consultaConsumoMedio60Dias @vFkResidenciaCdResidencia = 1;
+
+/* Procedure para consultar total consumo semanal */
+
+CREATE PROCEDURE sp_consultaTotalSemanal @vFkResidenciaCdResidencia INT
+AS
+SELECT
+	SUM(c.preco_consumo) as totalConsumo
+	
+FROM
+	Consumo as c
+INNER JOIN	
+	Dispositivo as d
+ON 
+	c.fk_Dispositivo_cd_dispositivo = d.cd_dispositivo
+WHERE	
+	d.fk_Residencia_cd_residencia = 1
+	AND 
+	c.data_consumo >= DATEADD(day, -7, GETDATE())
+
+EXEC sp_consultaTotalSemanal @vFkResidenciaCdResidencia = 1;
+
+/* Procedure para consultar relatório de gasto semanal */
+
+CREATE PROCEDURE sp_consultaRelatorioSemanal @vFkResidenciaCdResidencia INT
+AS
+SELECT
+	d.nome_dispositivo as nomeDispositivo,
+	c.data_consumo as dataConsumo,
+	c.preco_consumo as valorConsumo
+FROM
+	Consumo as c
+INNER JOIN	
+	Dispositivo as d
+ON 
+	c.fk_Dispositivo_cd_dispositivo = d.cd_dispositivo
+WHERE	
+	d.fk_Residencia_cd_residencia = 1
+	AND 
+	c.data_consumo >= DATEADD(day, -7, GETDATE())
+GROUP BY 
+	d.nome_dispositivo,
+	c.data_consumo,
+	c.preco_consumo
+
+	
+
+EXEC sp_consultaRelatorioSemanal @vFkResidenciaCdResidencia = 1;
+
+
+
+
+
+
+
