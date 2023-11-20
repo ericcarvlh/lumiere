1d390c319381b46c31bf956d6db0c1c85932faea
Eric Carvalho <ericcarvalho935@gmail.com>
2023-11-19 23:08:04 -0300

    Alteração no código fonte

--- lumiere-mvc/src/main/java/com/lumiere/boot/domain/RankingConsumidor.java
+++ lumiere-mvc/src/main/java/com/lumiere/boot/domain/RankingConsumidor.java
@@ -7,20 +7,10 @@ import jakarta.persistence.*;
         classes = @ConstructorResult(
                 targetClass = RankingConsumidor.class,
                 columns = {
-                    @ColumnResult(name = "consumoTotal", type = Double.class)
+                    @ColumnResult(name = "", type = Double.class)
         })
 )
 @Entity
 public class RankingConsumidor {
-	@Id
-	private int id;
-	
-	@Column(name = "ano")
-    private int ano;
-	
-	@Column(name = "nomeDispositivo")
-    private String nomeDispositivo;
 
-	@Column(name = "consumoTotal")
-	private double consumoTotal;
 }
